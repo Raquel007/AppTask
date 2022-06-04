bc60e5cfa9d6651a299d3b0808bf293eb247ff18
Raquel007 <mraquel_cavagnaro@hotmail.com>
2022-06-04 02:04:12 -0300

    Primer commit

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,16 @@
+import {Task} from "./Task"
+export const TASKS: Task [] = [
+    { 
+        id: 1,
+        text: "Terminar rimer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Hacer las compras",
+        day: 'Agusto 6',
+        reminder: true,
+    }
+
+]
